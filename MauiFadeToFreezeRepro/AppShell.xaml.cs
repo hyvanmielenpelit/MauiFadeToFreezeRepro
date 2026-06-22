478e1f1014f69f62e8777efccb2a0276baf57214
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2026-06-22 15:36:52 +0300

    Initial commit: Minimal reproduction of .NET MAUI 10.0.60 iOS FadeToAsync freeze

---
+++ MauiFadeToFreezeRepro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiFadeToFreezeRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiFadeToFreezeRepro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFadeToFreezeRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
